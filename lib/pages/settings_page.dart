03c35c9e5899771c0d6e45a746dee75cd094513c
Ekomobong Isaiah John <ekomzy@gmail.com>
2025-08-13 00:10:02 +0100

    added dark mode

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,7 @@
+import 'package:chat_app/theme/theme_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -7,6 +10,28 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text("Settings"))),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Dark Mode"),
+
+            CupertinoSwitch(
+              value: Provider.of<ThemeProvider>(context).isDarkMode,
+              onChanged: (value) => Provider.of<ThemeProvider>(
+                context,
+                listen: false,
+              ).toggleTheme(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
